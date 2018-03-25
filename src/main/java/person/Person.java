f6370fb6577773cc80e91dd53222e1bae0c46df0
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-03-25 21:07:34 +0530

    code re-formatted and this refference added to getter methods

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -13,15 +13,27 @@ public class Person {
 	private Integer age;
 	private String name;
 
-	public Integer getId() {return id;}
-	
-	public Integer getAge() {return age;}
+	public Integer getId() {
+		return this.id;
+	}
 
-	public String getName() {return name;}
-	
-	public void setId(Integer id) {this.id = id;}
+	public Integer getAge() {
+		return this.age;
+	}
 
-	public void setAge(Integer age) {this.age = age;}
+	public String getName() {
+		return this.name;
+	}
 
-	public void setName(String name) {this.name = name;}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
