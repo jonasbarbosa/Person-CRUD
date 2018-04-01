209e12c47053953341cdf2e1cd2fa73f0db0a58c
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-04-01 20:06:08 +0530

    imports re-ordered

--- src/main/java/person/PersonRestController.java
+++ src/main/java/person/PersonRestController.java
@@ -1,5 +1,7 @@
 package person;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @RestController
 public class PersonRestController {
